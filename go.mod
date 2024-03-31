d0f55b22891da7efce00375d6acb47446eab66fe
Raul <r.cornejo003@ebc.edu.mx>
2024-03-31 17:32:56 -0600

    coding method to refresh token was implemented according to the tutorial video

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module authenticator
 
 go 1.22.0
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible  // indirect
--- handlers.go
+++ handlers.go
@@ -27,9 +27,9 @@ type Claims struct {
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
-	var credentials = Credentials
+	var credentials = Credentials{}
 
-	err := json.NewDecoder(r.body).Decode(&credentials)
+	err := json.NewDecoder(r.Body).Decode(&credentials)
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -38,12 +38,12 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 	expectedPassword, ok := users[credentials.Username]
 
-	if !ok || expectedPassword != credentials.password {
+	if !ok || expectedPassword != credentials.Password {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
 
-	expirationTime := time.Now().add(time.Minute * 5)
+	expirationTime := time.Now().Add(time.Minute * 5)
 
 	claims := &Claims{
 		Username: credentials.Username,
@@ -105,5 +105,59 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func Refresh(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	tokenStr := cookie.Value
+
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tokenStr, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > 30*time.Second {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	expirationTime := time.Now().Add(time.Minute * 5)
+
+	claims.ExpiresAt = expirationTime.Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w,
+		&http.Cookie{
+			Name:    "refresh_token",
+			Value:   tokenString,
+			Expires: expirationTime,
+		})
 }
